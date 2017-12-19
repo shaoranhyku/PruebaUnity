6741a0c3e83e34180116881b526bfb122b80d813
DESKTOP-AAJQ9FR\Usuario <shaoranhyku@gmail.com>
2017-12-19 09:45:21 +0100

    First Commit.

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public float speed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed);
+	}
+}
